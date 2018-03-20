0f0d78d7a92359582dd15f439f42dcce6a431109
Greg Schultz <gjs86@cornell.edu>
2018-03-19 23:04:27 -0400

    initia;lize

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+<footer>
+  <p class = "left"><a class = "footer-link" href ="https://www.facebook.com/CUSustainableDesign/">Facebook</a> <a class = "footer-link" href ="https://www.cusd.cornell.edu">Website</a> <a class = "footer-link" href ="">Instagram</a> </p>
+  <p class = "right">
+  <span>&copy;&nbsp;2011&ndash;2018 Cornell University Sustainable Design</span></p>
+</footer>
---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js"> <!--<![endif]-->
+
+  <head>
+    <?php include "includes/header-core.php"; ?>
+  </head>
+
+  <body id="index">
+    <!--[if lt IE 7]>
+      <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+    <?php include "includes/navbar.php"; ?>
+
+     <div class="row center-align main">
+       <!-- main content here -->
+     </div>
+
+
+    <?php include "includes/footer.php"; ?>
+
+
+    <?php include "includes/js-scripts.php"; ?>
+
+
+  </body>
+
+</html>

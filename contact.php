e31ed4eb10639423d72800a64054b28547b252fe
Paris Korman <psk84@cornell.edu>
2018-03-22 09:56:17 -0400

    contact form

--- contact.php
+++ contact.php
@@ -7,6 +7,14 @@
 
   <head>
     <?php include "includes/header-core.php"; ?>
+    <?php
+        //creates versions for each time the style sheet is updated
+        $style_path = 'styles/styling.css';
+        $version = filemtime($style_path);
+        echo "<link rel='stylesheet' href='$style_path?ver=$version'>";
+    ?>         
+    <!-- captcha --> 
+    <!--<script src='https://www.google.com/recaptcha/api.js'></script>--> 
   </head>
 
   <body id="index">
@@ -17,7 +25,76 @@
 
      <div class="row center-align main">
        <!-- contact page here -->
-     </div>
+
+        <!-- captcha info:
+        site key= 6LdvMyAUAAAAAKuAIWJ1EuqXpHDxPVet-iNlAgJz
+        secret key= 6LdvMyAUAAAAAN5FG8WokcV1bSvHY7smLGGUNOEy
+        documentation at https://www.google.com/recaptcha/admin#site/337654639?setup
+         -->
+
+         <div>
+            <h2>Contact Us</h2>
+
+            <?php
+            if (isset($_POST['submit_msg'])){
+                  //$response = json_decode(file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=6LdvMyAUAAAAAN5FG8WokcV1bSvHY7smLGGUNOEy&response=".$_POST['g-recaptcha-response']));
+
+                  //if ($response->success){ //https://www.google.com/recaptcha/admin#site/337654639?setup -->
+
+                      if( !empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['message'])){
+                          $error = "";
+                      }
+                      else{
+                          $error="Please fill in all fields: ";
+                      }
+
+                      $receiver = "psk84@cornell.edu"; //always sent to Zeyu's email
+                      $sender = filter_input(INPUT_POST,'email', FILTER_SANITIZE_EMAIL);
+                      $name = filter_input(INPUT_POST,'name', FILTER_SANITIZE_STRING);
+                      $message = filter_input(INPUT_POST,'message', FILTER_SANITIZE_STRING);
+
+                      if (strlen($name) < 2){
+                          $error.="Please enter your name.  ";
+                      }
+                      if (preg_match("/[^a-zA-Z\-\s]/", $name)){
+                          $error.="Your name contains illegal characters.  ";
+                      }
+
+                      if (preg_match("/[^a-zA-Z0-9\.\,\:\!\-\s]/",$message)){
+                          $error.="The message field contains illegal characters.  ";
+                      }
+
+                      if(strlen($error)<1){
+                          $subject = "REDO Contact Form Submission";
+                          $body = "Message from ".$name.":  ".$message;
+                          $headers = "Reply to ".$name." at ".$sender;
+                          mail($receiver,$subject,$body,$headers);
+                          print("Your form has been submitted.");
+                      }
+                      else{
+                          print("<p class='error'>$error</p>");
+                      }
+                  }
+                  //else{
+                  //    print("Please confirm that you are not a robot.");
+                  //}
+
+
+               // end 'if form was submitted'
+          ?>
+
+                <form class="cospo" action='contact.php' method='post' enctype='multipart/form-data'>
+                    <input type="text" name="name" placeholder="Name">
+                    <input type="email" name="email" placeholder="Email">
+                    <textarea name="message" placeholder="Message" rows="5"></textarea>
+                    <!-- captcha -->
+                    <!--<div class="g-recaptcha" data-sitekey="6LeGfyUUAAAAAKlgeEycfo03lcAPkbELBL-zKahM"></div>--> 
+                    <input class='button' type='submit' name='submit_msg'>
+                </form>
+            </div>
+
+          </div>
+          
 
 
     <?php include "includes/footer.php"; ?>

00d21d0eff20aa07bb027c8f5fd331b9ec6870b9
Greg Schultz <gjs86@cornell.edu>
2018-03-26 15:04:56 -0400

    facts generator

--- js/index.js
+++ js/index.js
@@ -2,6 +2,17 @@ $(document).ready(function () {
   $('.parallax').parallax();
   $('.sidenav').sidenav();
   $(".dropdown-button").dropdown();  
+  $('.tap-target').tapTarget();
 });
 
 
+$("#facts_generator").on('click', function(){ 
+  alert(random_fact());
+});
+
+function random_fact(){
+  console.log("generating random fact");
+  let facts = [1,2,3,4,"zeyu sucks"];
+  let randomly_picked = facts[Math.floor((Math.random() * facts.length))];
+  return randomly_picked
+}

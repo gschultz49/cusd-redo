aab76b63cf6dbb7ac68c6ba1f5aa7f88cc5cb53f
Greg Schultz <gjs86@cornell.edu>
2018-04-07 16:14:54 -0400

    fixed contact page

--- contact.php
+++ contact.php
@@ -1,137 +1,72 @@
+<?php
+
+// for changing team leads...
+$teamLead= "Zeyu Hu";
+$teamLeadEmail = "zh75@cornell.edu";
+$teamLeadImg = "img/zeyu.jpeg";
+?>
+
 <!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!-->
 <html class="no-js"> <!--<![endif]-->
-
   <head>
     <?php include "includes/header-core.php"; ?>
-    <!-- captcha -->
-    <!--<script src='https://www.google.com/recaptcha/api.js'></script>-->
   </head>
-
-  <body id="index">
+  <body id="contact">
     <!--[if lt IE 7]>
       <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
     <![endif]-->
     <?php include "includes/navbar.php"; ?>
+    <?php //this page's content goes here ?>
 
-     <div class="row center-align main">
-       <!-- contact page here -->
+     <div class="container">
+       <div class="row lead">
+         <div class="col s12 m6">
+           <img class="lead-img" src= "<?php echo $teamLeadImg; ?>" alt="<?php echo $teamLead; ?>">
+         </div>
+         <div class="col s12 m6">
+           <h1> <?php echo $teamLead; ?> </h1>
+           <p>Composting is the natural process of recycling organic materials into nutrient rich soil amendment. Our team wishes to harvest the ample organic matter (AKA animal excrement) produced by the Smithsonian Zoo and use it to its full potential. Through this partnership, REDO is developing innovative solutions for zoos across the nation to handle their animal manure and food waste. We view waste not as a problem, but as a valued asset with environmental, economic, social, and educational worth. </p>
 
-        <?php
-        //  captcha info:
-        // site key= 6LdvMyAUAAAAAKuAIWJ1EuqXpHDxPVet-iNlAgJz
-        // secret key= 6LdvMyAUAAAAAN5FG8WokcV1bSvHY7smLGGUNOEy
-        // documentation at https://www.google.com/recaptcha/admin#site/337654639?setup
-         ?>
-
-         <div class="section white">
-            <h2>Contact Us</h2>
-
-            <?php
-            if (isset($_POST['submit_msg'])){
-                  //$response = json_decode(file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=6LdvMyAUAAAAAN5FG8WokcV1bSvHY7smLGGUNOEy&response=".$_POST['g-recaptcha-response']));
-
-                  //if ($response->success){ //https://www.google.com/recaptcha/admin#site/337654639?setup -->
-
-                      if( !empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['message'])){
-                          $error = "";
-                      }
-                      else{
-                          $error="Please fill in all fields: ";
-                      }
-
-                      $receiver = "psk84@cornell.edu";
-                    //   $receiver = "zh75@cornell.edu";
-                      $sender = filter_input(INPUT_POST,'email', FILTER_SANITIZE_EMAIL);
-                      $name = filter_input(INPUT_POST,'name', FILTER_SANITIZE_STRING);
-                      $message = filter_input(INPUT_POST,'message', FILTER_SANITIZE_STRING);
-
-                      if (strlen($name) < 2){
-                          $error.="Please enter your name.  ";
-                      }
-                      if (preg_match("/[^a-zA-Z\-\s]/", $name)){
-                          $error.="Your name contains illegal characters.  ";
-                      }
-
-                      if (preg_match("/[^a-zA-Z0-9\.\,\:\!\-\s]/",$message)){
-                          $error.="The message field contains illegal characters.  ";
-                      }
-
-                      if(strlen($error)<1){
-                          $subject = "REDO Contact Form Submission";
-                          $body = "Message from ".$name.":  ".$message;
-                          $headers = "Reply to ".$name." at ".$sender;
-                          mail($receiver,$subject,$body,$headers);
-                          print("Your form has been submitted, we'll get back to you soon!.");
-                      }
-                      else{
-                          print("<p class='error'>$error</p>");
-                      }
-                  }
-                  //else{
-                  //    print("Please confirm that you are not a robot.");
-                  //}
-
-
-               // end 'if form was submitted'
-          ?>
-                <div class="row container zeyu">
-                    <img src="img/zeyu.jpeg">
-                    <h5 class="center-align">Hi, I'm Zeyu Hu, the team lead of REDO. Curious about our work? Email me below!</h5>
-                </div>
-
-                <div class="row container">
-                    <form class="cospo" action='contact.php' method='post' enctype='multipart/form-data'>
-                        <div class="input-field col s12 l6">
-
-                            <div class="input-field col s12">
-                                <input id ="text_name" type="text" name="name" >
-                                <label for="text_name">Name</label>
-                            </div>
-
-                            <div class="input-field col s12">
-                                <input id ="text_email" type="email" name="email" >
-                                <label for="text_email">Email</label>
-                            </div>
-                        </div>
-                        <div class="input-field col s12 l6">
-
-                            <div class="input-field col s12">
-                                <textarea id="text_message" class= "materialize-textarea" name="message" data-length="120"></textarea>
-
-                                <label for="text_message">Message</label>
-                            </div>
-
-                            <div class="input-field col s12">
-                                <input class='btn' type='submit' name='submit_msg'>
-                                <!-- captcha -->
-                                <!--<div class="g-recaptcha" data-sitekey="6LeGfyUUAAAAAKlgeEycfo03lcAPkbELBL-zKahM"></div>-->
-                            </div>
-                        </div>
-
-
-                    </form>
-                </div>
-            </div>
-
-
-
-
-            </div>
-
-          </div>
-
-
-
-    <?php include "includes/footer.php"; ?>
-
-
+            <p>Any questions? Feel free to reach out to me at<a href="mailto:<?php echo $teamLeadEmail; ?>"> <?php echo $teamLeadEmail; ?></a></p>
+         </div>
+       </div>
+       <div class="row grid">
+         <div class="col s12 m4 member">
+           <img class="member-img" src= "img/zeyu.jpeg" alt="MEMBER">
+           <p> Kenneth Young </p>
+           <p>Package Designer </p>
+         </div>
+         <div class="col s12 m4 member">
+           <img class="member-img" src= "img/zeyu.jpeg" alt="MEMBER">
+           <p> Kenneth Young </p>
+           <p>Package Designer </p>
+         </div>
+         <div class="col s12 m4 member">
+           <img class="member-img" src= "img/zeyu.jpeg" alt="MEMBER">
+           <p> Kenneth Young </p>
+           <p>Package Designer </p>
+         </div>
+         <div class="col s12 m4 member">
+           <img class="member-img" src= "img/zeyu.jpeg" alt="MEMBER">
+           <p> Kenneth Young </p>
+           <p>Package Designer </p>
+         </div>
+         <div class="col s12 m4 member">
+           <img class="member-img" src= "img/zeyu.jpeg" alt="MEMBER">
+           <p> Kenneth Young </p>
+           <p>Package Designer </p>
+         </div>
+         <div class="col s12 m4 member">
+           <img class="member-img" src= "img/zeyu.jpeg" alt="MEMBER">
+           <p> Kenneth Young </p>
+           <p>Package Designer </p>
+         </div>
+       </div>
+     </div>
     <?php include "includes/js-scripts.php"; ?>
-
-
   </body>
-
 </html>
--- includes/navbar.php
+++ includes/navbar.php
@@ -3,19 +3,13 @@
   <div id ="navbar">
     <a href="index.php" class="brand-logo hide-on-med-and-down"><img class ="redo-logo" src = "img/redo-lego.png"></a>
     <a href="#" data-target="mobile-menu" class="sidenav-trigger hide-on-med-and-up"><i class="material-icons">menu</i></a>
-    <?php
-
-    //figure out how to render a centered logo on small screens instead of just links
-
-    // <a href="index.php" class="brand-logo left"><img class ="redo-logo" src = "img/redo-lego.png"></a>
-    ?>
     <ul class="links right hide-on-med-and-down ">
       <li> <a id="index" href="index.php">Home</a></li>
       <li> <a id="mikey" href="mikey.php">Mikey</a></li>
       <li> <a id="compost" href="compost.php">Compost</a></li>
       <li> <a id="education" href="education.php">Education</a></li>
       <li> <a id="infographic" href="infographic.php">Infographic</a></li>
-      <li> <a id="contact2" href="contact2.php">Contact</a></li>
+      <li> <a id="contact" href="contact.php">Contact</a></li>
     </ul>
   </div>
 </div>
@@ -26,5 +20,5 @@
   <li> <a id="compost" href="compost.php">Compost</a></li>
   <li> <a id="education" href="education.php">Education</a></li>
   <li> <a id="infographic" href="infographic.php">Infographic</a></li>
-  <li> <a id="contact2" href="contact2.php">Contact</a></li>
+  <li> <a id="contact" href="contact.php">Contact</a></li>
 </ul>

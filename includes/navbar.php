61f0923101408f4c71a1a4cc937ab10cdaff38c5
Greg Schultz <gjs86@cornell.edu>
2018-04-07 17:26:05 -0400

    w3 validation

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,7 +1,7 @@
 <?php //core navbar HTML ?>
 <div class="nav-wrapper">
   <div id ="navbar">
-    <a href="index.php" class="brand-logo hide-on-med-and-down"><img class ="redo-logo" src = "img/redo-lego.png"></a>
+    <a href="index.php" class="brand-logo hide-on-med-and-down"><img class ="redo-logo" src = "img/redo-lego.png" alt="LOGO"></a>
     <a href="#" data-target="mobile-menu" class="sidenav-trigger hide-on-med-and-up"><i class="material-icons">menu</i></a>
     <ul class="links right hide-on-med-and-down ">
       <li> <a id="index" href="index.php">Home</a></li>
@@ -15,10 +15,10 @@
 </div>
 
 <ul class="sidenav" id="mobile-menu">
-  <li> <a id="index" href="index.php">Home</a></li>
-  <li> <a id="mikey" href="mikey.php">Mikey</a></li>
-  <li> <a id="compost" href="compost.php">Compost</a></li>
-  <li> <a id="education" href="education.php">Education</a></li>
-  <li> <a id="infographic" href="infographic.php">Infographic</a></li>
-  <li> <a id="contact" href="contact.php">Contact</a></li>
+  <li><a href="index.php">Home</a></li>
+  <li><a href="mikey.php">Mikey</a></li>
+  <li><a href="compost.php">Compost</a></li>
+  <li><a href="education.php">Education</a></li>
+  <li><a href="infographic.php">Infographic</a></li>
+  <li><a href="contact.php">Contact</a></li>
 </ul>
--- infographic.php
+++ infographic.php
@@ -3,11 +3,11 @@
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!-->
-<html class="no-js"> <!--<![endif]-->
+<html lang="en" class="no-js"> <!--<![endif]-->
   <head>
     <?php include "includes/header-core.php"; ?>
   </head>
-  <body id="infographic">
+  <body id="infographic-page">
     <!--[if lt IE 7]>
       <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
     <![endif]-->

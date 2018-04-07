cba3c46a8a6d569e87f1a2b54dbb2641d3d1da61
Greg Schultz <gjs86@cornell.edu>
2018-04-07 16:45:36 -0400

    finished mikey page

--- mikey.php
+++ mikey.php
@@ -7,17 +7,38 @@
   <head>
     <?php include "includes/header-core.php"; ?>
   </head>
-  <body id="index">
+  <body id="mikey">
     <!--[if lt IE 7]>
       <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
     <![endif]-->
     <?php include "includes/navbar.php"; ?>
     <?php //this page's content goes here ?>
+    <div class="container">
 
-     <div class="container">
-        <h1 class="center-align"> da fuk goes on this page </h1>
-     </div>
-     <?php include "includes/footer.php"; ?>
+      <div class="row">
+        <div class="shadow"><img class="mikey" src="img/mikey.png" alt="MIKEY"></div>
+      </div>
+
+      <div class="row">
+        <div class="shadow"><img class="leaves" src="img/leaves.png" alt="LEAVES"></div>
+      </div>
+
+      <div class="row center-align">
+        <div class="col s12 m6 offset-m3">
+          <p> First, organic waste is diverted from landfills and collected for composting. </p>
+        </div>
+        <div class="col s12 m6 offset-m3">
+          <p>Compost is made up of “browns” and “greens.” Green material is nitrogen rich, and I can use nitrogen to grow and reproduce. Brown material is carbon rich, and carbon is my main source of energy.</p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="shadow"><img class="mikey-info" src="img/mikey-info.png" alt="MIKEY-INFO"></div>
+      </div>
+
+
+    </div>
+    <?php include "includes/footer.php"; ?>
     <?php include "includes/js-scripts.php"; ?>
   </body>
 </html>

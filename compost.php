0b2410d8f3b81c8f3d09a9a86a2fa0649136d95f
Greg Schultz <gjs86@cornell.edu>
2018-04-07 15:32:07 -0400

    compost page

--- compost.php
+++ compost.php
@@ -4,30 +4,43 @@
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!-->
 <html class="no-js"> <!--<![endif]-->
-
   <head>
     <?php include "includes/header-core.php"; ?>
   </head>
-
-  <body id="index">
+  <body id="compost">
     <!--[if lt IE 7]>
       <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
     <![endif]-->
     <?php include "includes/navbar.php"; ?>
-
-     <div class="section white">
-      <div class="row container">
-        <h2 class="header">Who are we?</h2>
-        <p class="grey-text text-darken-3 lighten-3">Composting is the natural process of recycling organic materials into nutrient rich soil amendment. Our team wishes to harvest the ample organic matter (AKA animal excrement) produced by the Smithsonian Zoo and use it to its full potential. Through this partnership, REDO is developing innovative solutions for zoos across the nation to handle their animal manure and food waste. We view waste not as a problem, but as a valued asset with environmental, economic, social, and educational worth.</p>
+    <?php //this page's content goes here ?>
+    <div class="container">
+      <div class="row">
+        <div class="shadow"><img class="house" src="img/house.png" alt="HOUSE"></div>
       </div>
     </div>
-
-    <?php include "includes/footer.php"; ?>
-
-
+    <div class="row center-align">
+      <h1> What can you do at home? </h1>
+      <div class="row">
+        <div class="col s12 m6 offset-m3">
+          <p>On average, Americans throw out over four pounds of waste everyday, but there is so much you can do at home to minimize your impact! If you remember to reduce, reuse, recycle, and compost at home, then you can do your part to help keep the planet healthy. </p>
+        </div>
+      </div>
+      <div class="shadow"><img class="compost-info" src="img/compost-info.png" alt="compost-info1"></div>
+      <div class="row">
+        <div class="col s12 m6 offset-m3">
+          <b> Indoor vs Outdoor </b>
+          <br>
+           <p><span> Indoor -  </span> Buy a bin, throw food scraps, let it sit. If managed well, there should be no smells.</p></span>
+          <p><span> Outdoor - </span> Make a pile, throw food scraps, turn pile, keep it moist</p></span>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col s12 m6 offset-m3">
+          <p>Many items that usually get thrown out can actually be recycled. Recycling keeps waste out of landfills, oceans, and other natural habitats where it can be harmful to the ecosystems. Recycling allows waste to get turned into new products like furntature, bottles, and many more. Many towns and cities will pick up recycling or have designated locations to drop off recycling. </p>
+        </div>
+      </div>
+      <div class="shadow"><img class="compost-info2" src="img/compost-info2.png" alt="compost-info2"></div>
+    </div>
     <?php include "includes/js-scripts.php"; ?>
-
-
   </body>
-
 </html>

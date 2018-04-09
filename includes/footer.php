2be7bc0369eb38030e61486e50282208032d771a
Paris Korman <psk84@cornell.edu>
2018-04-08 22:21:15 -0400

    reformat footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,18 +1,22 @@
 <footer id="footer" class="page-footer">
    <div class="container">
      <div class="row">
-       <div class="col l6 s12">
-         <p>This is site is created by REDO, a sub team of Cornell University Sustainable Design. The content on this website does not reflect, in anyway, the interest or positions of the Smithsonian Institute. For more information please visit the !!!!!!!! page.</p>
+      
+       <!-- <div class="col l8 s12">-->
+        <h5> REDO </h5>
+         <p>This is site is created by REDO, a sub-team of Cornell University Sustainable Design. The content on this website does not reflect in any way the interest or positions of the Smithsonian Institute. For more information please visit the !!!!!!!! page.</p>
+        <a href="#!"><img class="icon" src="img/facebook.png" alt="Facebook"></a>
+        <a href="#!"><img class="icon" src="img/insta.png" alt="Instagram"></a>
+         <ul><li><a href="https://cusd.cornell.edu/">&copy;&nbsp;2011&ndash;<?php echo date("o")?> Cornell University Sustainable Design</a></li></ul>
        </div>
-       <div class="col l5 offset-l1 s12">
-         <!-- <h5 class="white-text">&copy;&nbsp;2011&ndash;<?php echo date("o")?> Cornell University Sustainable Design</h5> -->
+       <!--<div class="foot-col col l3 offset-l1 s12 ">
+         <!-- <h5 class="white-text">&copy;&nbsp;2011&ndash;<?php echo date("o")?> Cornell University Sustainable Design</h5>
          <ul>
-           <li><a href="https://cusd.cornell.edu/">&copy;&nbsp;2011&ndash;<?php echo date("o")?> Cornell University Sustainable Design</a></li>
-           <!-- <li><a href="#!">Link 2</a></li>
-           <li><a href="#!">Link 3</a></li>
-           <li><a href="#!">Link 4</a></li> -->
+            <li><a href="#!">Facebook</a></li>
+           <li><a href="#!">Twitter</a></li>
+           <li><a href="#!">Link 4</a></li>
          </ul>
-       </div>
+       </div>-->
      </div>
    </div>
  </footer>

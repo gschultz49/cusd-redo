cb3b511cfb34cb945551073fcefd95b18c599bf5
Greg Schultz <gjs86@cornell.edu>
2018-04-18 14:58:32 -0400

    re input css

--- includes/header-core.php
+++ includes/header-core.php
@@ -10,4 +10,5 @@
 <meta name="description" content="">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
+<link rel="stylesheet" href="css/style.css">
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
